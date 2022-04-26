6eff7e7b2d37783072c67517daf114c0bcf7c865
ekalinauskas <ekalinauskas@gmail.com>
2022-04-26 18:38:09 +0300

    Finnished

---
+++ LearningArrays/LearningArrays.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LearningArrays
+{
+    class LearningArrays
+    {
+        static void Main(string[] args)
+        {
+            // Task1:
+            // Step 1: Array Declaration
+            string[] sarasas;
+
+            // Step 2:Array Initialization
+            sarasas = new string[5] { "Antanas", "Petras", "Jonas", "Aloyzas", "Martynas" };
+
+            //Step 3: Loop "for"
+            Console.Write("For loop: ");
+            for (int j = 0; j < sarasas.Length; j++) Console.Write(" " + sarasas[j]);
+
+            {
+                Console.WriteLine();
+            }
+
+            //Task2:
+            //Step 1: Array Declaration
+
+            int[] skaiciai;
+
+            // Step 2:Array Initialization
+
+            skaiciai = new int[21] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Array.Reverse(skaiciai);
+
+            //Step 3: Loop "for" backwards
+            Console.Write("Skaiciai loop: ");
+            for (int i = 0; i < skaiciai.Length; i++) Console.Write(" " + skaiciai[i]);
+
+            {
+                //Console.WriteLine(skaiciai[i]);
+            }
+        }
+    }
+}
